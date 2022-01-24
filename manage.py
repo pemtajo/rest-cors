c1d782dd233c4adb5d3f399c5039ec64023dc5ac
Pedro Matias de Araujo <pmatias@ibm.com>
2022-01-24 20:07:54 -0300

    changes
    Signed-off-by: Pedro Matias de Araujo <pmatias@ibm.com>

--- manage.py
+++ manage.py
@@ -48,4 +48,4 @@ def url_informations():
         return {'error': str(exp)}, 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=os.getenv("APPLICATION_PORT", 5000))
+    app.run(host="0.0.0.0", debug=True, port=os.getenv("PORT", 5000))
